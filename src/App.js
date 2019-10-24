961be90ad9735657f9ca5e200ce86f1c43bdcfe9
vsanjeevagowda@qwinix.io <vsanjeevagowda@qwinix.io>
2019-10-24 20:25:00 +0000

    first

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const initialValue = Value.fromJSON({
         nodes: [
           {
             object: 'text',
-            text: '',
+            text: 'A line of text in a paragraph.',
           },
         ],
       },
@@ -21,113 +21,71 @@ const initialValue = Value.fromJSON({
   },
 })
 
-function CodeNode(props) {
-  return (
-    <pre {...props.attributes}>
-      <code className='code-block'>{props.children}</code>
-    </pre>
-  )
-};
+function MarkHotkey(options) {
+  const { type, key } = options
 
-// Define a React component to render bold text with.
-function BoldMark(props) {
-  return <strong>{props.children}</strong>
+  // Return our "plugin" object, containing the `onKeyDown` handler.
+  return {
+    onKeyDown(event, editor, next) {
+      // If it doesn't match our `key`, let other plugins handle it.
+      if (!event.ctrlKey || event.key != key) return next()
+
+      // Prevent the default characters from being inserted.
+      event.preventDefault()
+
+      // Toggle the mark `type`.
+      editor.toggleMark(type)
+    },
+  }
 }
 
-// Define our app...
+// Initialize our bold-mark-adding plugin.
+const boldPlugin = MarkHotkey({
+  type: 'bold',
+  key: 'b',
+})
+
+// Create an array of plugins.
+const plugins = [boldPlugin]
+
 class App extends React.Component {
-  // Set the initial value when the app is first constructed.
   state = {
     value: initialValue,
   }
 
-  // On change, update the app's React state with the new editor value.
   onChange = ({ value }) => {
-    console.log('[value]', value)
     this.setState({ value })
   }
 
-  // // Define a new handler which prints the key that was pressed.
-  // onKeyDown = (event, editor, next) => {
-  //   // Return with no changes if the keypress is not '&'
-  //   if (event.key !== '&') return next()
-
-  //   // Prevent the ampersand character from being inserted.
-  //   event.preventDefault()
-  //   // Change the value by inserting 'and' at the cursor's position.
-  //   editor.insertText('and')
-  // }
-
-//   onKeyDown = (event, editor, next) => {
-//     console.log('event.key', event.key)
-//     // Return with no changes if it's not the "`" key with ctrl pressed.
-    
-//     if (event.key !== '`' || !event.altKey) return next()
-//     debugger
-//     // if (!event.ctrlKey) return next()
-
-//     // Prevent the "`" from being inserted by default.
-//     event.preventDefault()
-// debugger
-//     // Determine whether any of the currently selected blocks are code blocks.
-//     const isCode = editor.value.blocks.some(block => block.type === 'code')
-// debugger
-//     // Toggle the block type depending on `isCode`.
-//     editor.setBlocks(isCode ? 'paragraph' : 'code')
-//   }
-
-onKeyDown = (event, editor, next) => {
-  if (!event.ctrlKey) return next()
-
-  switch (event.key) {
-    case 'b': {
-      event.preventDefault()
-      editor.toggleMark('bold')
-      break
-    }
-    case '`': {
-      const isCode = editor.value.blocks.some(block => block.type === 'code')
-      event.preventDefault()
-      editor.setBlocks(isCode ? 'paragraph' : 'code')
-      break
-    }
-    default: {
-      return next()
-    }
-  }
-}
-
-  // Render the editor.
   render() {
-    const { value } = this.state;
-    console.log('[initialValue]', value)
     return (
-    <Editor 
-    value={value} 
-    onChange={this.onChange} 
-    onKeyDown={this.onKeyDown}
-    renderBlock={this.renderBlock} 
-    renderMark={this.renderMark}/>)
+      // Add the `plugins` property to the editor, and remove `onKeyDown`.
+      <Editor
+        plugins={plugins}
+        value={this.state.value}
+        onChange={this.onChange}
+        renderMark={this.renderMark}
+      />
+    )
   }
 
-  renderBlock = (props, editor, next) => {
-    switch (props.node.type) {
+  renderMark = (props, editor, next) => {
+    switch (props.mark.type) {
+      case 'bold':
+        return <strong>{props.children}</strong>
+      // Add our new mark renderers...
       case 'code':
-        return <CodeNode {...props} />
+        return <code>{props.children}</code>
+      case 'italic':
+        return <em>{props.children}</em>
+      case 'strikethrough':
+        return <del>{props.children}</del>
+      case 'underline':
+        return <u>{props.children}</u>
       default:
         return next()
     }
-  };
-
-    // Add a `renderMark` method to render marks.
-    renderMark = (props, editor, next) => {
-      switch (props.mark.type) {
-        case 'bold':
-          return <BoldMark {...props} />
-        default:
-          return next()
-      }
-    }
+  }
 }
 
 export default App;
