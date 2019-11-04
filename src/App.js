94246f4d281eae14d0d7679698222e63fad3d88d
vsanjeevagowda@qwinix.io <vsanjeevagowda@qwinix.io>
2019-11-04 19:55:09 +0000

    working editor

--- src/App.js
+++ src/App.js
@@ -1,92 +1,218 @@
-import React from 'react';
-import { Editor } from 'slate-react';
-import { Value } from 'slate';
+import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Editor } from 'slate-react';
+import initialValue from './value.json'
+import { Value } from 'slate';
+import { isKeyHotkey } from 'is-hotkey';
+import { Button } from 'reactstrap';
 
-// Create our initial value...
-const initialValue = Value.fromJSON({
-  document: {
-    nodes: [
-      {
-        object: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            object: 'text',
-            text: 'A line of text in a paragraph.',
-          },
-        ],
-      },
-    ],
-  },
-})
+const isBoldHotkey = isKeyHotkey('mod+b');
+const isItalicHotkey = isKeyHotkey('mod+i');
+const isUnderlinedHotkey = isKeyHotkey('mod+u');
+const isCodeHotkey = isKeyHotkey('mod+`');
+const DEFAULT_NODE = 'paragraph';
 
-function MarkHotkey(options) {
-  debugger
-  const { type, key } = options
+class App extends Component {
 
-  // Return our "plugin" object, containing the `onKeyDown` handler.
-  return {
-    onKeyDown(event, editor, next) {
-      // If it doesn't match our `key`, let other plugins handle it.
-      if (!event.ctrlKey || event.key != key) return next()
-
-      // Prevent the default characters from being inserted.
-      event.preventDefault()
-
-      // Toggle the mark `type`.
-      editor.toggleMark(type)
-    },
-  }
-}
-
-// Initialize our bold-mark-adding plugin.
-const boldPlugin = MarkHotkey({
-  type: 'bold',
-  key: 'b',
-})
-
-// Create an array of plugins.
-const plugins = [boldPlugin]
-
-class App extends React.Component {
   state = {
-    value: initialValue,
+    value: Value.fromJSON(initialValue),
   }
 
   onChange = ({ value }) => {
     this.setState({ value })
+  };
+
+  onKeyDown = (event, editor, next) => {
+    let mark
+
+    if (isBoldHotkey(event)) {
+      mark = 'bold'
+    } else if (isItalicHotkey(event)) {
+      mark = 'italic'
+    } else if (isUnderlinedHotkey(event)) {
+      mark = 'underlined'
+    } else if (isCodeHotkey(event)) {
+      mark = 'code'
+    } else {
+      return next()
+    }
+
+    event.preventDefault()
+    editor.toggleMark(mark)
+  }
+
+  ref = editor => {
+    this.editor = editor
+  };
+
+  hasMark = type => {
+    const { value } = this.state
+    return value.activeMarks.some(mark => mark.type === type)
+  }
+
+  renderBlock = (props, editor, next) => {
+    const { attributes, children, node } = props
+
+    switch (node.type) {
+      case 'block-quote':
+        return <blockquote {...attributes}>{children}</blockquote>
+      case 'bulleted-list':
+        return <ul {...attributes}>{children}</ul>
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>
+      case 'list-item':
+        return <li {...attributes}>{children}</li>
+      case 'numbered-list':
+        return <ol className='ml-4' {...attributes}>{children}</ol>
+      default:
+        return next()
+    }
+  };
+
+  renderMark = (props, editor, next) => {
+    const { children, mark, attributes } = props
+
+    switch (mark.type) {
+      case 'bold':
+        return <strong {...attributes}>{children}</strong>
+      case 'code':
+        return <code {...attributes}>{children}</code>
+      case 'italic':
+        return <em {...attributes}>{children}</em>
+      case 'underlined':
+        return <u {...attributes}>{children}</u>
+      default:
+        return next()
+    }
+  };
+
+  renderMarkButton = (type, className) => {
+    const isActive = this.hasMark(type)
+
+    return (
+      <Button
+        className='cursor-pointer border-0'
+        active={isActive}
+        size='sm'
+        outline color='secondary'
+        onMouseDown={event => this.onClickMark(event, type)}
+      >
+        {/* <Icon>{icon}</Icon> */}
+        <i className={className} />
+      </Button>
+    )
+  };
+
+  renderBlockButton = (type, className) => {
+    let isActive = this.hasBlock(type)
+
+    if (['numbered-list', 'bulleted-list'].includes(type)) {
+      const { value: { document, blocks } } = this.state
+
+      if (blocks.size > 0) {
+        const parent = document.getParent(blocks.first().key)
+        isActive = this.hasBlock('list-item') && parent && parent.type === type
+      }
+    }
+
+    return (
+      <Button
+        className='cursor-pointer border-0'
+        active={isActive}
+        size='sm'
+        outline color='secondary'
+        onMouseDown={event => this.onClickBlock(event, type)}
+      >
+        <i className={className}/>
+      </Button>
+    )
+  };
+
+  onClickMark = (event, type) => {
+    event.preventDefault()
+    this.editor.toggleMark(type)
+  }
+
+  hasBlock = type => {
+    const { value } = this.state
+    return value.blocks.some(node => node.type === type)
+  }
+
+  onClickBlock = (event, type) => {
+    event.preventDefault()
+
+    const { editor } = this
+    const { value } = editor
+    const { document } = value
+
+    // Handle everything but list buttons.
+    if (type !== 'bulleted-list' && type !== 'numbered-list') {
+      const isActive = this.hasBlock(type)
+      const isList = this.hasBlock('list-item')
+
+      if (isList) {
+        editor
+          .setBlocks(isActive ? DEFAULT_NODE : type)
+          .unwrapBlock('bulleted-list')
+          .unwrapBlock('numbered-list')
+      } else {
+        editor.setBlocks(isActive ? DEFAULT_NODE : type)
+      }
+    } else {
+      // Handle the extra wrapping required for list buttons.
+      const isList = this.hasBlock('list-item')
+      const isType = value.blocks.some(block => {
+        return !!document.getClosest(block.key, parent => parent.type === type)
+      })
+
+      if (isList && isType) {
+        editor
+          .setBlocks(DEFAULT_NODE)
+          .unwrapBlock('bulleted-list')
+          .unwrapBlock('numbered-list')
+      } else if (isList) {
+        editor
+          .unwrapBlock(
+            type === 'bulleted-list' ? 'numbered-list' : 'bulleted-list'
+          )
+          .wrapBlock(type)
+      } else {
+        editor.setBlocks('list-item').wrapBlock(type)
+      }
+    }
   }
 
   render() {
     return (
-      // Add the `plugins` property to the editor, and remove `onKeyDown`.
-      <Editor
-        plugins={plugins}
-        value={this.state.value}
-        onChange={this.onChange}
-        renderMark={this.renderMark}
-      />
+      <div className='editor-div p-2'>
+        <div className='border-bottom'>
+          {this.renderMarkButton('bold', 'fa fa-bold')}
+          {this.renderMarkButton('italic', 'fa fa-italic')}
+          {this.renderMarkButton('underlined', 'fa fa-underline')}
+          {this.renderMarkButton('code', 'fa fa-code')}
+          {this.renderBlockButton('heading-one', 'fa fa-heading1')}
+          {this.renderBlockButton('heading-two', 'fa fa-heading2')}
+          {this.renderBlockButton('numbered-list', 'fa fa-list-ol')}
+          {this.renderBlockButton('bulleted-list', 'format_list_bulleted')}
+        </div>
+        <Editor
+          spellCheck
+          autoFocus
+          placeholder="Enter some rich text..."
+          ref={this.ref}
+          value={this.state.value}
+          onChange={this.onChange}
+          onKeyDown={this.onKeyDown}
+          renderBlock={this.renderBlock}
+          renderMark={this.renderMark}
+        />
+      </div>
     )
-  }
+  };
 
-  renderMark = (props, editor, next) => {
-    switch (props.mark.type) {
-      case 'bold':
-        return <strong>{props.children}</strong>
-      // Add our new mark renderers...
-      case 'code':
-        return <code>{props.children}</code>
-      case 'italic':
-        return <em>{props.children}</em>
-      case 'strikethrough':
-        return <del>{props.children}</del>
-      case 'underline':
-        return <u>{props.children}</u>
-      default:
-        return next()
-    }
-  }
 }
-
 export default App;
